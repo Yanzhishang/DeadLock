68a606e90be6df0c9524c888e880a49804da08ae
Yanzhishang <1176377464@qq.com>
2017-05-08 09:03:00 +0800

    。

--- src/com/yzs/deadlock/DeadLockTest1.java
+++ src/com/yzs/deadlock/DeadLockTest1.java
@@ -1,14 +1,19 @@
 package com.yzs.deadlock;
+
 /**
  * 资源抢占发生死锁
+ * 
  * @author YZS
  */
 public class DeadLockTest1 {
 	public static void main(String[] args) {
-
-		// 定义两个资源
+		/**
+		 * 两个线程都占有了一个资源之后，想要再去占有另一个资源，都只能等待对方释放资源之后才能再去占有。所以两个想成都处于等待状态，这就产生了死锁。
+		 */
+		// 资源:牛奶和面包
 		Object resource1 = "面包";
 		Object resource2 = "牛奶";
+
 		// 第一个线程thread1，想先占有面包,再尝试占有牛奶
 		Thread thread1 = new Thread() {
 			public void run() {
@@ -16,16 +21,15 @@ public class DeadLockTest1 {
 				synchronized (resource1) {
 					// 成功持有面包
 					System.out.println("Thread1:占有了  " + resource1);
-					// 休眠一段时间
 					try {
+						// 休眠一下下，让另一个线程有足够的时间去占有另一个线程
 						Thread.sleep(50);
 					} catch (Exception e) {
+
 					}
 					// thread2去占有牛奶,如果不能占有，thread1将会一直等待
 					synchronized (resource2) {
-
 						System.out.println("Thread1:占有了  " + resource2);
-
 					}
 				}
 			}
@@ -38,10 +42,10 @@ public class DeadLockTest1 {
 				synchronized (resource2) {
 					// 成功占有牛奶
 					System.out.println("Thread2:占有了  " + resource2);
-					// 休眠一段时间
 					try {
 						Thread.sleep(50);
 					} catch (Exception e) {
+
 					}
 					// thread2去占有面包,如果不能占有，thread2将会一直等待
 					synchronized (resource1) {
